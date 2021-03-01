cf928b6437565f13aefe39f977d501cf1e1415ce
AlexPawelczyk <alex.pawelczyk@uwaterloo.ca>
2021-03-01 00:36:42 -0500

    Initialize countries dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core";
 
 function App() {
   return (
     <div className="app">
-      <h1>COVID-19 Tracker</h1>
-    </div>
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries and show dropdown list of the options */}
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">YOOOOOOOOO</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+      
+
+
+      {/* PROJECT STRUCTURE */}
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>    
   );
 }
 
